58fefefab4db04c3019b8a9be8d2b7596658ab7c
Your Name <meshackkemboi011@gmail.com>
2022-10-18 09:18:55 +0300

    created the post method for the quotes

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
+const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(bodyParser.urlencoded({ extended: true}))
+
+app.get('/', (req, res) =>
+ res.sendFile(__dirname + '/index.html'))
+
+app.get('/', (req, res) => {/*...*/})
+app.post('/qotes', (req, res) => {/*...*/})
+
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 
